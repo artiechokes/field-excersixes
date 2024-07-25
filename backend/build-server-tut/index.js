f7f64a0fda04da76a9c0fe5146feeee7668b3841
Arturo J. Laínez <arturojlainez@gmail.com>
2024-07-24 22:15:27 -0400

    Added HTTP Requests

--- backend/build-server-tut/index.js
+++ backend/build-server-tut/index.js
@@ -6,6 +6,23 @@ app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1>");
 });
 
+app.post("/register", (req, res) => {
+  //Do something with data
+  res.sendStatus(201);
+});
+
+app.put("/user/angela", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.patch("/user/angela", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.delete("/user/angela", (req, res) => {
+  res.sendStatus(200);
+});
+
 app.get("/about", (req, res) => {
   res.send(`<h2>About Me</h2><p>Hello! I’m Arturo J. Laínez, a Web Developer and Designer with four years of experience crafting dynamic, user-friendly websites. I specialize in responsive design and development using HTML, CSS, JavaScript, Node.js, Adobe XD, and Figma. I manage projects from concept to completion, ensuring attention to detail and clean code.
 
