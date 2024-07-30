4f5cc514a185c6bdff753e8cde80fddff94b1f89
Arturo J. Lainez <arturojlainez@gmail.com>
2024-07-29 22:38:53 -0400

    Created Middleware Function

--- backend/build-server-tut/index.js
+++ backend/build-server-tut/index.js
@@ -4,13 +4,15 @@ const app = express();
 const port = 3000;
 app.use(morgan("dev"));
 
-app.use((req, res, next) => {
+function logger(req, res, next) {
   console.log(`Request Method: ${req.method}`);
   console.log(`Request URL: ${req.url}`);
   console.log(`Request Date: ${new Date()}`);
 
   next();
-});
+}
+
+app.use(logger);
 
 app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1>");
